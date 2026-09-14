d0e471317b57decc89646aa0177c419c100d2fbd
Pugachev Anton <antonpugachev913@gmail.com>
2026-09-14 12:56:46 +0300

    test date function

--- src/widget.py
+++ src/widget.py
@@ -16,6 +16,17 @@ def mask_account_card(data : str) -> str:
         return f'{final_name} {number[:4]} {number[4:6]}** **** {number[12:]}'
 
 
-print(mask_account_card('Счет 73654108430135874305'))
-print(mask_account_card('Visa Platinum 7000792289606361'))
-print(mask_account_card('Maestro 1596837868705199'))
+# print(mask_account_card('Счет 73654108430135874305'))
+# print(mask_account_card('Visa Platinum 7000792289606361'))
+# print(mask_account_card('Maestro 1596837868705199'))
+
+
+def get_date(date : str) -> str:
+    """Функция, изменяющая формат даты"""
+    year = date[:4]
+    month = date[5:7]
+    day = date[8:10]
+    ans = f'{day}.{month}.{year}'
+    return ans
+
+# print(get_date("2024-03-11T02:26:18.671407"))
