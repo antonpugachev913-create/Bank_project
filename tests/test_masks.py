d4c2ff958d76cdf3ad8540e80eb843892deeed2a
unknown <antonpugachev913@gmail.com>
2026-09-21 15:09:29 +0300

    update tests_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,8 +3,6 @@ from src.masks import get_mask_card_number
 
 def test_correct_mask_card():
     assert get_mask_card_number("7000792289606361") == '7000 79** **** 6361'
-    assert get_mask_card_number('') == 'Incorrect length string'
-
     with pytest.raises(TypeError):
          assert get_mask_card_number(123)
     with pytest.raises(TypeError):
@@ -12,5 +10,6 @@ def test_correct_mask_card():
 
 def test_correct_mask_card_len():
     assert get_mask_card_number("70007922896063611") == 'Incorrect length string'
+    assert get_mask_card_number('') == 'Incorrect length string'
 
     # print(get_mask_account("73654108430135874305"))
