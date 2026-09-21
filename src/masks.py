cb0b050be47e54505b8c0c4ffae992a793b0abb5
unknown <antonpugachev913@gmail.com>
2026-09-21 15:07:50 +0300

    update masks.py and add test on this function

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,9 @@
 def get_mask_card_number(number: str) -> str:
     """Функция маскировки номера карты"""
+    if not isinstance(number, str):
+        raise TypeError
+    if len(number) != 16:
+        return 'Incorrect length string'
     return f'{number[:4]} {number[4:6]}** **** {number[12:]}'
 
 
@@ -8,6 +12,6 @@ def get_mask_account(number: str) -> str:
     return f"** {number[-4:]}"
 
 
-if __name__ == "__main__":
-    print(get_mask_card_number("7000792289606361"))
-    print(get_mask_account("73654108430135874305"))
+# if __name__ == "__main__":
+    # print(get_mask_card_number("7000792289606361"))
+    # print(get_mask_account("73654108430135874305"))
---
+++ tests/test_masks.py
@@ -0,0 +1,16 @@
+import pytest
+from src.masks import get_mask_card_number
+
+def test_correct_mask_card():
+    assert get_mask_card_number("7000792289606361") == '7000 79** **** 6361'
+    assert get_mask_card_number('') == 'Incorrect length string'
+
+    with pytest.raises(TypeError):
+         assert get_mask_card_number(123)
+    with pytest.raises(TypeError):
+        assert get_mask_card_number([1, 2, 3, 4, 5])
+
+def test_correct_mask_card_len():
+    assert get_mask_card_number("70007922896063611") == 'Incorrect length string'
+
+    # print(get_mask_account("73654108430135874305"))
