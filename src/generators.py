9a7476d44d91c882c9f435f0630c6a5818d87cdb
unknown <antonpugachev913@gmail.com>
2026-09-24 08:32:37 +0300

    small fixes

--- src/generators.py
+++ src/generators.py
@@ -1,7 +1,10 @@
-from typing import Any
 from collections.abc import Iterator
+from typing import Any
 
-def filter_by_currency(transactions: list[dict[str, Any]], currency:str = '') -> Iterator[dict[str, Any]]:
+
+def filter_by_currency(
+    transactions: list[dict[str, Any]], currency: str = ""
+) -> Iterator[dict[str, Any]]:
     """Функция, возвращающая итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной"""
     for operation in transactions:
         try:
@@ -18,18 +21,15 @@ def transaction_descriptions(transactions: list[dict[str, Any]]) -> Iterator[str
 
 
 #
-def card_number_generator(start: int, end : int) -> Iterator[str]:
-    """Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX """
-    for num in range(start, end+1):
+def card_number_generator(start: int, end: int) -> Iterator[str]:
+    """Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX"""
+    for num in range(start, end + 1):
         str_num = str(num)
-        l = len(str_num)
-        card_num = ('0' * (16 - l)) + str_num
-        final_num = f'{card_num[:4]} {card_num[4:8]} {card_num[8:12]} {card_num[12:]}'
+        length = len(str_num)
+        card_num = ("0" * (16 - length)) + str_num
+        final_num = f"{card_num[:4]} {card_num[4:8]} {card_num[8:12]} {card_num[12:]}"
         yield final_num
-
-
 
 
 for card_number in card_number_generator(1, 5):
     print(card_number)
-
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,20 +1,25 @@
 import pytest
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+from src.generators import (
+    card_number_generator,
+    filter_by_currency,
+    transaction_descriptions,
+)
 
 
 def test_filter_by_currency(check_filter):
-    usd_iterator = filter_by_currency(check_filter, 'USD')
+    usd_iterator = filter_by_currency(check_filter, "USD")
     assert next(usd_iterator) == {
         "id": 939719570,
         "state": "EXECUTED",
         "date": "2018-06-30T02:08:58.425572",
         "operationAmount": {
             "amount": "9824.07",
-            "currency": {"name": "USD", "code": "USD"}
+            "currency": {"name": "USD", "code": "USD"},
         },
         "description": "Перевод организации",
         "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702"
+        "to": "Счет 11776614605963066702",
     }
     assert next(usd_iterator) == {
         "id": 142264268,
@@ -22,11 +27,11 @@ def test_filter_by_currency(check_filter):
         "date": "2019-04-04T23:20:05.206878",
         "operationAmount": {
             "amount": "79114.93",
-            "currency": {"name": "USD", "code": "USD"}
+            "currency": {"name": "USD", "code": "USD"},
         },
         "description": "Перевод со счета на счет",
         "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188"
+        "to": "Счет 75651667383060284188",
     }
     assert next(usd_iterator) == {
         "id": 895315941,
@@ -34,18 +39,18 @@ def test_filter_by_currency(check_filter):
         "date": "2018-08-19T04:27:37.904916",
         "operationAmount": {
             "amount": "56883.54",
-            "currency": {"name": "USD", "code": "USD"}
+            "currency": {"name": "USD", "code": "USD"},
         },
         "description": "Перевод с карты на карту",
         "from": "Visa Classic 6831982476737658",
-        "to": "Visa Platinum 8990922113665229"
+        "to": "Visa Platinum 8990922113665229",
     }
 
 
 def test_filter_zero(check_filter):
-    usd_iterator = filter_by_currency(check_filter, 'OMG')
-    assert next(usd_iterator, 'Bad') == 'Bad'
-    assert next(usd_iterator, 'Bad') == 'Bad'
+    usd_iterator = filter_by_currency(check_filter, "OMG")
+    assert next(usd_iterator, "Bad") == "Bad"
+    assert next(usd_iterator, "Bad") == "Bad"
 
 
 def test_filter_no():
@@ -56,9 +61,9 @@ def test_filter_no():
 
 def test_transaction_descriptions(check_filter):
     us_iterator = transaction_descriptions(check_filter)
-    assert next(us_iterator) == 'Перевод организации'
-    assert next(us_iterator) == 'Перевод со счета на счет'
-    assert next(us_iterator) == 'Перевод со счета на счет'
+    assert next(us_iterator) == "Перевод организации"
+    assert next(us_iterator) == "Перевод со счета на счет"
+    assert next(us_iterator) == "Перевод со счета на счет"
 
 
 def test_transaction_descriptions_size():
@@ -66,14 +71,17 @@ def test_transaction_descriptions_size():
     with pytest.raises(StopIteration):
         next(us_iterator)
 
-@pytest.mark.parametrize ('index, another', [(0, "Перевод другу"),
-    (1, "Оплата подписки"),
-    (2, "Покупка продуктов"),
-    (3, "Выплата зарплаты"),
-    (4, "Описание отсутствует")
 
-])
-
+@pytest.mark.parametrize(
+    "index, another",
+    [
+        (0, "Перевод другу"),
+        (1, "Оплата подписки"),
+        (2, "Покупка продуктов"),
+        (3, "Выплата зарплаты"),
+        (4, "Описание отсутствует"),
+    ],
+)
 def test_transaction_description_big(index, another, description):
     text = description[index]
     usd_iterator = transaction_descriptions([text])
@@ -82,13 +90,9 @@ def test_transaction_description_big(index, another, description):
 
 def test_card_number_generator():
     iterator = card_number_generator(1, 5)
-    assert next(iterator) == '0000 0000 0000 0001'
-    assert next(iterator) == '0000 0000 0000 0002'
-    assert next(iterator) == '0000 0000 0000 0003'
-    assert next(iterator) == '0000 0000 0000 0004'
-    assert next(iterator) == '0000 0000 0000 0005'
-    assert next(iterator, 'Bad') == 'Bad'
-
-
-
-
+    assert next(iterator) == "0000 0000 0000 0001"
+    assert next(iterator) == "0000 0000 0000 0002"
+    assert next(iterator) == "0000 0000 0000 0003"
+    assert next(iterator) == "0000 0000 0000 0004"
+    assert next(iterator) == "0000 0000 0000 0005"
+    assert next(iterator, "Bad") == "Bad"
